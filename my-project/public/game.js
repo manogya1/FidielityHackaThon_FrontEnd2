a59817ef7d45a8cd97d4dc639305c365cc1040d8
osifalujoi1 <osifalujoi1@mymail.nku.edu>
2024-04-19 23:39:02 -0400

    more frontend page

--- my-project/public/game.js
+++ my-project/public/game.js
@@ -1,113 +1,112 @@
 $(document).ready(function() {
-    let currentPlayer = "X";
-    let board = ["", "", "", "", "", "", "", "", ""];
-    let isFirstRound = true;
-  
-    // Function to start the game
-    function startGame() {
-      $('#home_container').fadeOut('slow', function() {
-        $('#game-container').fadeIn('slow');
-      });
-    }
-  
-    // Function to end the game and return to homepage
-    function endGame() {
-      $('#game-container').fadeOut('slow', function() {
-        $('#home_container').fadeIn('slow');
-      });
-    }
-  
-    // Function to handle player clicks on cells
-    function handleCellClick() {
-      const cellIndex = $(this).index();
-      if (board[cellIndex] === "") {
-        board[cellIndex] = currentPlayer;
-        $(this).text(currentPlayer);
-        if (checkWin(currentPlayer)) {
-          alert(currentPlayer + " wins!");
-          resetGame();
-        } else if (checkDraw()) {
-          alert("It's a draw!");
-          resetGame();
-        } else {
-          currentPlayer = currentPlayer === "X" ? "O" : "X";
-        }
+  let currentPlayer = "X";
+  let board = ["", "", "", "", "", "", "", "", ""];
+  let isFirstRound = true;
+
+  // Function to start the game
+  function startGame() {
+    $('#home_container').fadeOut('slow', function() {
+      $('#game-container').fadeIn('slow');
+    });
+  }
+
+  // Function to end the game and return to homepage
+  function endGame() {
+    $('#game-container').fadeOut('slow', function() {
+      $('#home_container').fadeIn('slow');
+    });
+  }
+
+  // Function to handle player clicks on cells
+  function handleCellClick() {
+    const cellIndex = $(this).index();
+    if (board[cellIndex] === "") {
+      board[cellIndex] = currentPlayer;
+      $(this).text(currentPlayer);
+      if (checkWin(currentPlayer)) {
+        alert(currentPlayer + " wins!");
+        resetGame();
+      } else if (checkDraw()) {
+        alert("It's a draw!");
+        resetGame();
+      } else {
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
       }
     }
-  
-    // Function to check for a win
-    function checkWin(player) {
-      const winCombinations = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-      ];
-      return winCombinations.some(combination => {
-        return combination.every(index => {
-          return board[index] === player;
-        });
+  }
+
+  // Function to check for a win
+  function checkWin(player) {
+    const winCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    return winCombinations.some(combination => {
+      return combination.every(index => {
+        return board[index] === player;
       });
-    }
-  
-    // Function to check for a draw
-    function checkDraw() {
-      return board.every(cell => {
-        return cell !== "";
-      });
-    }
-  
-    // Function to reset the game
-    function resetGame() {
-      board = ["", "", "", "", "", "", "", "", ""];
-      $('.cell').text("");
-      currentPlayer = "X";
-      showRandomFact();
-      if (isFirstRound) {
-        $('#reset-button').prop('disabled', false);
-        isFirstRound = false;
-      }
-    }
-  
-    // Function to show a random financial fact
-    function showRandomFact() {
-      const facts = [
-        "Fact 1: Financial literacy can empower individuals to make informed financial decisions.",
-        "Fact 2: Saving even small amounts regularly can lead to significant financial growth over time.",
-        // Add more facts here...
-      ];
-  
-      const randomFact = facts[Math.floor(Math.random() * facts.length)];
-      alert(randomFact);
-      showPopup();
-    }
-  
-    // Function to show the pop-up
-    function showPopup() {
-      $('#popup').fadeIn('slow');
-    }
-  
-    // Event listener for the continue button in the pop-up
-    $('#continue-button').click(function() {
-      $('#popup').fadeOut('slow');
     });
-  
-    // Event listener for the reset button
-    $('#reset-button').click(function() {
-      resetGame();
+  }
+
+  // Function to check for a draw
+  function checkDraw() {
+    return board.every(cell => {
+      return cell !== "";
     });
-  
-    // Event listener for the play button
-    $('#play_button').click(startGame);
-  
-    // Event listener for the back to home button
-    $('#back-to-home').click(endGame);
-  
-    // Event listener for cell clicks
-    $('.cell').click(handleCellClick);
+  }
+
+  // Function to reset the game
+  function resetGame() {
+    board = ["", "", "", "", "", "", "", "", ""];
+    $('.cell').text("");
+    currentPlayer = "X";
+    showRandomFact();
+    if (isFirstRound) {
+      $('#reset-button').prop('disabled', false);
+      isFirstRound = false;
+    }
+  }
+
+  // Function to show a random financial fact
+  function showRandomFact() {
+    const facts = [
+      "Fact 1: Financial literacy can empower individuals to make informed financial decisions.",
+      "Fact 2: Saving even small amounts regularly can lead to significant financial growth over time.",
+      // Add more facts here...
+    ];
+
+    const randomFact = facts[Math.floor(Math.random() * facts.length)];
+    alert(randomFact);
+    showPopup();
+  }
+
+  // Function to show the pop-up
+  function showPopup() {
+    $('#popup').fadeIn('slow');
+  }
+
+  // Event listener for the continue button in the pop-up
+  $('#continue-button').click(function() {
+    $('#popup').fadeOut('slow');
   });
-  
+
+  // Event listener for the reset button
+  $('#reset-button').click(function() {
+    resetGame();
+  });
+
+  // Event listener for the play button
+  $('#play_button').click(startGame);
+
+  // Event listener for the back to home button
+  $('#back-to-home').click(endGame);
+
+  // Event listener for cell clicks
+  $('.cell').click(handleCellClick);
+});
--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,5 +1,3 @@
-import $ from 'jquery';
-
 $(document).ready(function() {
     $('#home_button').click(function() {
       $('#home_container').fadeOut('slow', function() {
@@ -13,6 +11,86 @@ $(document).ready(function() {
     $('#back_to_home').click(function() {
       $('#concept_page').fadeOut('slow', function() {
         $('#home_container').fadeIn('slow');
+        $('html, body').animate({
+          scrollTop: $('#concept_page').offset().top
+        }, 'slow');
+      });
+    });
+  
+    //lesson 1
+    $('#next_button').click(function() {
+      $('#concept_page').fadeOut('slow', function() {
+        $('#lesson1_container').fadeIn('slow');
+        $('html, body').animate({
+          scrollTop: $('#lesson1_container').offset().top
+        }, 'slow');
+      });
+    });
+  
+    $('#back_to_concept').click(function() {
+      $('#lesson1_container').fadeOut('slow', function() {
+        $('#concept_page').fadeIn('slow');
+        $('html, body').animate({
+          scrollTop: $('#concept_page').offset().top
+        }, 'slow');
+      });
+    });
+  
+    //lesson 2
+    $('#next_button2').click(function() {
+      $('#lesson1_container').fadeOut('slow', function() {
+        $('#lesson2_container').fadeIn('slow');
+        $('html, body').animate({
+          scrollTop: $('#lesson2_container').offset().top
+        }, 'slow');
+      });
+    });
+  
+    $('#back_to_concept1').click(function() {
+      $('#lesson2_container').fadeOut('slow', function() {
+        $('#lesson1_container').fadeIn('slow');
+        $('html, body').animate({
+          scrollTop: $('#lesson1_container').offset().top
+        }, 'slow');
+      });
+    });
+  
+    //lesson 3
+    $('#next_button3').click(function() {
+      $('#lesson2_container').fadeOut('slow', function() {
+        $('#lesson3_container').fadeIn('slow');
+        $('html, body').animate({
+          scrollTop: $('#lesson3_container').offset().top
+        }, 'slow');
+      });
+    });
+  
+    $('#back_to_concept2').click(function() {
+      $('#lesson3_container').fadeOut('slow', function() {
+        $('#lesson2_container').fadeIn('slow');
+        $('html, body').animate({
+          scrollTop: $('#lesson2_container').offset().top
+        }, 'slow');
+      });
+    });
+  
+    //lesson4
+    $('#next_button4').click(function() {
+      $('#lesson3_container').fadeOut('slow', function() {
+        $('#lesson4_container').fadeIn('slow');
+        $('html, body').animate({
+          scrollTop: $('#lesson4_container').offset().top
+        }, 'slow');
+      });
+    });
+  
+    $('#back_to_concept3').click(function() {
+      $('#lesson4_container').fadeOut('slow', function() {
+        $('#lesson3_container').fadeIn('slow');
+        $('html, body').animate({
+          scrollTop: $('#lesson3_container').offset().top
+        }, 'slow');
       });
     });
   });
+  
