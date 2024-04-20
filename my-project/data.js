f692ca12875c532135b7b5cc357478132060e5e1
osifalujoi1 <osifalujoi1@mymail.nku.edu>
2024-04-20 11:33:58 -0400

    title change

--- my-project/data.js
+++ my-project/data.js
@@ -1,50 +1,89 @@
 const lessonData = {
     "lessons": [
         {
-            "ID": "1",
-            "title": "Lesson 1: Colors",
-            "content": "In this lesson, we will learn about different colors such as red, blue, yellow, and green.",   
-            "questions": [
-                {
-                    "question": "What color is the sky?",
-                    "answer": "Blue"
-                },
-                {
-                    "question": "Which color is a banana?",
-                    "answer": "Yellow"
-                }
+            "id": "lesson1_container",
+            "children": [
+              { "tag": "a", "id": "back_to_concept", "href": "#", "class": "back-button", "text": "&larr;Go Back" },
+              { "tag": "h2", "class": "title", "id": "Lesson1", "text": "Lesson One" },
+              {
+                "class": "content",
+                "id": "lesson_content",
+                "children": [
+                  { "tag": "h1", "id": "concept_title", "children": [{ "tag": "b", "text": "What is Money?" }] },
+                  {
+                    "class": "lesson",
+                    "children": [
+                      { "tag": "p", "text": "<lesson1_paragraph>" }
+                    ]
+                  }
+                ]
+              },
+              { "tag": "button", "class": "button", "id": "next_button2", "text": "Next" }
             ]
-        },
-        {
-            "ID": "2",
-            "title": "Lesson 2: Animals",
-            "content": "Today, we will explore different animals like lions, elephants, and birds.",
-            "questions": [
-                {
-                    "question": "What animal has a trunk and tusks?",
-                    "answer": "Elephant"
-                },
-                {
-                    "question": "Which animal roars in the jungle?",
-                    "answer": "Lion"
-                }
+          },
+          {
+            "id": "lesson2_container",
+            "children": [
+              { "tag": "a", "id": "back_to_concept1", "href": "#", "class": "back-button", "text": "&larr; Back to Lesson 1" },
+              { "tag": "h2", "class": "title", "id": "Lesson2", "text": "Lesson Two" },
+              {
+                "class": "content",
+                "id": "lesson_content",
+                "children": [
+                  { "tag": "h1", "id": "concept_title", "children": [{ "tag": "b", "text": "Why we save" }] },
+                  {
+                    "class": "lesson",
+                    "children": [
+                      { "tag": "p", "text": "<lesson2_paragraph>" }
+                    ]
+                  }
+                ]
+              },
+              { "tag": "button", "class": "button", "id": "next_button3", "text": "Next" }
             ]
-        },
-        {
-            "ID": "3",
-            "title": "Lesson 3: Shapes",
-            "content": "Let's discover shapes like circles, squares, triangles, and rectangles.",
-            "questions": [
-                {
-                    "question": "What shape has three sides?",
-                    "answer": "Triangle"
-                },
-                {
-                    "question": "Which shape looks like a box?",
-                    "answer": "Square"
-                }
+          },
+          {
+            "id": "lesson3_container",
+            "children": [
+              { "tag": "a", "id": "back_to_concept2", "href": "#", "class": "back-button", "text": "&larr; Back to Lesson 2" },
+              { "tag": "h2", "class": "title", "id": "Lesson3", "text": "Lesson Third" },
+              {
+                "class": "content",
+                "id": "lesson_content",
+                "children": [
+                  { "tag": "h1", "id": "concept_title", "children": [{ "tag": "b", "text": "A good day for money" }] },
+                  {
+                    "class": "lesson",
+                    "children": [
+                      { "tag": "p", "text": "<lesson3_paragraph>" }
+                    ]
+                  }
+                ]
+              },
+              { "tag": "button", "class": "button", "id": "next_button4", "text": "Next" }
             ]
-        }
+          },
+          {
+            "id": "lesson4_container",
+            "children": [
+              { "tag": "a", "id": "back_to_concept3", "href": "#", "class": "back-button", "text": "&larr; Back to Lesson 3" },
+              { "tag": "h2", "class": "title", "id": "Lesson4", "text": "Lesson Four" },
+              {
+                "class": "content",
+                "id": "lesson_content",
+                "children": [
+                  { "tag": "h1", "id": "concept_title", "children": [{ "tag": "b", "text": "Saving makes us wait" }] },
+                  {
+                    "class": "lesson",
+                    "children": [
+                      { "tag": "p", "text": "<lesson4_paragraph>" }
+                    ]
+                  }
+                ]
+              },
+              { "tag": "button", "class": "button", "id": "next_button5", "text": "Next" }
+            ]
+          },
     ]
 };
 
